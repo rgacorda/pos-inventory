62001c954041beeb8a10bc42de533d147a99e639
Ronjay Acorda <rgacorda.the2nd@gmail.com>
2026-04-16 19:48:31 +0800

    backend app listen bug fix for prod

--- apps/backend-api/src/main.ts
+++ apps/backend-api/src/main.ts
@@ -25,7 +25,7 @@ async function bootstrap() {
   );
 
   const port = process.env.PORT ?? 3000;
-  await app.listen(port);
-  console.log(`🚀 Backend API running on http://localhost:${port}`);
+  await app.listen(port, '0.0.0.0');
+  console.log(`🚀 Backend API running on http://0.0.0.0:${port}`);
 }
 bootstrap();
